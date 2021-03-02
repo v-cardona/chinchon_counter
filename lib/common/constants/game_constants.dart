7de2aad4d73594270042223909bf417165755b72
Victor Cardona <vicardona7@gmail.com>
2021-03-02 19:59:17 +0100

    changed max_puntuation constant

--- lib/common/constants/game_constants.dart
+++ lib/common/constants/game_constants.dart
@@ -6,6 +6,6 @@ class GameConstants {
   static const int min_lifes = 1;
   static const int max_lifes = 5;
   static const int dead_player = -1;
-  static const int max_puntuation = 99; // inclusive
-  static const int lifes_lost = 0; // inclusive
+  static const int max_puntuation = 100; // inclusive
+  static const int lifes_lost = 0;
 }
